bdf3e438d69f9af2f3092d85961f85b5e00a8796
Alan Gutierrez <alan@prettyrobots.com>
2016-08-09 23:40:23 -0500

    Remove test sub-directory.

--- t/translate.t.js
+++ t/translate.t.js
@@ -1,7 +1,7 @@
 require('proof')(1, prove)
 
 function prove (assert) {
-    var string = 'abc', encoders = require('../..')
+    var string = 'abc', encoders = require('..')
     string = encoders.utf8.decode(encoders.utf8.encode(string))
     assert(string, 'abc', 'translated')
 }
